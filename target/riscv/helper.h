e7b7eec2dfa4ec6372ce8c7ebf8f6e3483315ff9
Liren Wei <lrwei@bupt.edu.cn>
2021-06-10 12:46:20 +0800

    target/riscv: Explicitly mark noreturn helpers
    Including WFI.
    
    Signed-off-by: Liren Wei <lrwei@bupt.edu.cn>

--- target/riscv/helper.h
+++ target/riscv/helper.h
@@ -73,7 +73,7 @@ DEF_HELPER_4(csrrc, tl, env, tl, tl, tl)
 #ifndef CONFIG_USER_ONLY
 DEF_HELPER_2(sret, tl, env, tl)
 DEF_HELPER_2(mret, tl, env, tl)
-DEF_HELPER_1(wfi, void, env)
+DEF_HELPER_1(wfi, noreturn, env)
 DEF_HELPER_1(tlb_flush, void, env)
 #endif
 
