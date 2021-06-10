ce224ca686367db9a6e88a709924eb161288e095
Liren Wei <lrwei@bupt.edu.cn>
2021-06-10 12:00:44 +0800

    target/arm: Explicitly mark noreturn helpers
    Including EXCEPTION_{ INTERNAL, WITH_SYNDROME, BKPT_INSN }, WFE, and
    YIELD.
    
    Signed-off-by: Liren Wei <lrwei@bupt.edu.cn>

--- target/arm/helper.h
+++ target/arm/helper.h
@@ -44,13 +44,13 @@ DEF_HELPER_FLAGS_2(usad8, TCG_CALL_NO_RWG_SE, i32, i32, i32)
 
 DEF_HELPER_FLAGS_3(sel_flags, TCG_CALL_NO_RWG_SE,
                    i32, i32, i32, i32)
-DEF_HELPER_2(exception_internal, void, env, i32)
-DEF_HELPER_4(exception_with_syndrome, void, env, i32, i32, i32)
-DEF_HELPER_2(exception_bkpt_insn, void, env, i32)
+DEF_HELPER_2(exception_internal, noreturn, env, i32)
+DEF_HELPER_4(exception_with_syndrome, noreturn, env, i32, i32, i32)
+DEF_HELPER_2(exception_bkpt_insn, noreturn, env, i32)
 DEF_HELPER_1(setend, void, env)
 DEF_HELPER_2(wfi, void, env, i32)
-DEF_HELPER_1(wfe, void, env)
-DEF_HELPER_1(yield, void, env)
+DEF_HELPER_1(wfe, noreturn, env)
+DEF_HELPER_1(yield, noreturn, env)
 DEF_HELPER_1(pre_hvc, void, env)
 DEF_HELPER_2(pre_smc, void, env, i32)
 
